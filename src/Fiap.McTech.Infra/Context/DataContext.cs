5c097209ecf32674113638974d09a6880f6568a6
guilherme_novaes <guilherme.novaes233@gmail.com>
2024-05-04 00:21:54 -0300

    fix project and add dbset into DataContext

--- src/Fiap.McTech.Infra/Context/DataContext.cs
+++ src/Fiap.McTech.Infra/Context/DataContext.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Fiap.McTech.Domain.Entities.Cart;
+using Fiap.McTech.Domain.Entities.Catalogs;
+using Fiap.McTech.Domain.Entities.Clients;
+using Fiap.McTech.Domain.Entities.Orders;
+using Fiap.McTech.Domain.Entities.Payments;
+using Fiap.McTech.Domain.Entities.Products;
 
 namespace Fiap.McTech.Infra.Context
 {
@@ -9,6 +15,14 @@ namespace Fiap.McTech.Infra.Context
 		   : base(options)
 		{
 		}
+
+		public DbSet<CartClient> CartClients { get; set; }
+		public DbSet<CartItem> CartItems { get; set; }
+		public DbSet<Catalog> Catalog { get; set; }
+		public DbSet<Client> Clients { get; set; }
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<Payment> Payments { get; set; }
+		public DbSet<Product> Products { get; set; }
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
