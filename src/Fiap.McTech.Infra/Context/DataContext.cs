9ae15a5154c90fec9cc2a9ab74e26033a65e347c
guilherme_novaes <guilherme.novaes233@gmail.com>
2024-05-05 23:56:09 -0300

    add OrderItem into db set

--- src/Fiap.McTech.Infra/Context/DataContext.cs
+++ src/Fiap.McTech.Infra/Context/DataContext.cs
@@ -21,6 +21,7 @@ namespace Fiap.McTech.Infra.Context
 		public DbSet<Catalog> Catalog { get; set; }
 		public DbSet<Client> Clients { get; set; }
 		public DbSet<Order> Orders { get; set; }
+		public DbSet<OrderItem> OrderItems { get; set; }
 		public DbSet<Payment> Payments { get; set; }
 		public DbSet<Product> Products { get; set; }
 
